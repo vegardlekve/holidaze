222a77a6e3450556d3da56768a711685c750e681
vegardlekve <91474181+vegardlekve@users.noreply.github.com>
2023-05-28 23:03:13 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+// Components
+import Layout from "./components/layout";
+// Hooks
+import { AuthProvider } from "./hooks/context";
+//Pages
+import Home from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import Profile from "./pages/profile";
+import Venue from "./pages/venue";
+//Styles
+import GlobalStyle from "./styles/GlobalStyles";
+
+function App() {
+    return <div className="App"></div>;
+    return (
+        <AuthProvider>
+            <div className="App">
+                <GlobalStyle />
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="register" element={<Register />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="profile" element={<Profile />} />
+                        <Route path="/venue/:id" element={<Venue />} />
+                    </Route>
+                </Routes>
+            </div>
+        </AuthProvider>
+    );
+}
+
+export default App;
